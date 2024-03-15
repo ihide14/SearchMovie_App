c105985283ce165e7e5074369df211bb2356dc0a
ihide14 <juandiman26@gmal.com>
2024-03-14 22:06:02 -0500

    fixed https url

--- src/Hooks/useServices.js
+++ src/Hooks/useServices.js
@@ -4,7 +4,7 @@ export function useServices() {
 
     const API_KEY = "7725876f"
 
-    const API_URL = `http://www.omdbapi.com/?apikey=${API_KEY}&`
+    const API_URL = `https://www.omdbapi.com/?apikey=${API_KEY}&`
 
     const getMovie = (movie) => {
         const request = axios.get(`${API_URL}s=${movie}`)
