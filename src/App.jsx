5cbca9b1d81b30c84d724bc54f18150c1c3242d1
ihide14 <juandiman26@gmal.com>
2024-03-16 22:49:31 -0500

    add the sort by year button

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,31 @@
+import { useEffect, useState } from 'react'
 import MovieCard from './Components/Movie_card'
 import { useLookSearch } from './Hooks/useLookSearch'
 
 function App() {
 
   const { movies, search, error, lookSearch, verifiedSubmit } = useLookSearch()
+  const [sort, setSort] = useState(false)
+
+  useEffect(() => {
+
+    var sortSelector = document.getElementById("SortBotton")
+
+    if (movies === null) {
+      sortSelector.style.display = "none"
+      console.log(sortSelector)
+    }
+
+    else { sortSelector.style.display = "block" }
+
+
+  }, [movies])
+
+  function handleSort() {
+    setSort(!sort)
+  }
+
+  const sortedMovies = movies?.Results
 
   return (
     <>
@@ -12,12 +34,18 @@ function App() {
         <form onSubmit={verifiedSubmit}> Enter the movie you want to search for:
           <input style={{
             border: '1px solid transparent', borderColor: error ? 'red' : 'transparent'
-          }} onChange={lookSearch} value={search} placeholder='Look a movie....' /> <button>Search</button>
+          }} onChange={lookSearch} value={search} placeholder='Look a movie....' /> <input type='button' id='SortBotton' style={{
+            backgroundColor: sort ? 'green' : ''
+          }} value={"Sort by Year"} onClick={handleSort} />
+          <button>Search</button>
         </form>
         {error ? (`${error}`) : ("")}
       </header >
       <article className='DirectoryMovies'>
-        {movies && movies?.Results?.length > 0 ? (movies?.Results.map(dataMovie => <MovieCard movie={dataMovie} key={dataMovie.imdbID} />)) : ("")}
+        {movies && movies?.Results?.length > 0 ? (sort ? sortedMovies.slice().sort((a, b) => b.Year - a.Year).map(dataMovie => <MovieCard movie={dataMovie} key={dataMovie.imdbID} />) :
+          movies?.Results.map(dataMovie => <MovieCard movie={dataMovie} key={dataMovie.imdbID} />)
+        ) : ("")}
+
       </article>
     </>
   )
